f43772c1d9cf14ee40e7f5437490179eb749ccc9
Eros Ryan Simette <127696165+Eros-afk@users.noreply.github.com>
2025-01-28 21:23:55 +0000

    #

--- TrabalhoComArvores/main.c
+++ TrabalhoComArvores/main.c
@@ -5,19 +5,6 @@
 int main() {
     // Criando uma árvore binária de exemplo
     ArvB* arvore = arvb_cria_vazia();
-    arvore = arvb_insere(arvore, 10);
-    arvore = arvb_insere(arvore, 7);
-    arvore = arvb_insere(arvore, 15);
-    arvore = arvb_insere(arvore, 3);
-    arvore = arvb_insere(arvore, 8);
-    arvore = arvb_insere(arvore, 20);
-
-    // Contando as folhas primas
-    int resultado = folhas_primos(arvore);
-    printf("A árvore tem %d folhas primas, kawaii~!\n", resultado);
-
-    // Liberando a memória da árvore
-    arvb_libera(arvore);
-
+    
     return 0;
 }
